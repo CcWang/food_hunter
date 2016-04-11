d53f3c0a41a168dee9f1f2e71d73c1d8ce6ec5cc
CC Wang <chongchong1110@gmail.com>
2016-04-11 01:36:44 -0700

    category list

--- client/static/category_script.js
+++ client/static/category_script.js
@@ -6,9 +6,16 @@ myApp.factory('categoryFactory',function ($http,$location) {
 })
 
 myApp.controller('categoryController',function($scope, categoryFactory,mainFactory ){
+  $scope.category = [];
+  // get data.fav_category, try to put into a new map{counter:[category]}
   var getUser = function(data){
     $scope.user = data;
-    console.log($scope.user);
+    Object.keys(data.fav_category).forEach(function(key) {
+      var value = data.fav_category[key];
+      var cat = {};
+      cat[key] = value;
+      $scope.category.push(cat);
+    });
   }
   getUser(mainFactory.user);
 })
