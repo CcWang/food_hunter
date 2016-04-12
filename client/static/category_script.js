7fd98ffe495baca76705c503a0ad4ffb3c89bf7d
CC Wang <chongchong1110@gmail.com>
2016-04-11 23:55:25 -0700

    display category order by counter value

--- client/static/category_script.js
+++ client/static/category_script.js
@@ -10,12 +10,19 @@ myApp.controller('categoryController',function($scope, categoryFactory,mainFacto
   // get data.fav_category, try to put into a new map{counter:[category]}
   var getUser = function(data){
     $scope.user = data;
-    Object.keys(data.fav_category).forEach(function(key) {
-      var value = data.fav_category[key];
-      var cat = {};
-      cat[key] = value;
-      $scope.category.push(cat);
-    });
+    // Object.keys(data.fav_category).forEach(function(key) {
+    //   var value = data.fav_category[key];
+    //   var cat = {};
+    //   cat[key] = value;
+    //   $scope.category.push(cat);
+    // });
+    for (var cat in data.fav_category) {
+      $scope.category.push([cat,data.fav_category[cat]]);
+    }
+    $scope.category.sort(function(a,b){return b[1] - a[1]});
   }
   getUser(mainFactory.user);
+  $scope.update = function(name){
+    console.log(name);
+  }
 })
