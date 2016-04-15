e666183155e1e51a36e317c48a2037f3c12e6b19
CC Wang <chongchong1110@gmail.com>
2016-04-14 22:34:38 -0700

    uninstall googleapi

--- client/static/script.js
+++ client/static/script.js
@@ -14,22 +14,6 @@ myApp.config(function($routeProvider){
       redirectTo:'/'
     })
 })
-// myApp.factory('homeFactory',function($http){
-//   var factory = {};
-//   factory.getYelp = function () {
-//     console.log('factory')
-//     $http.get('/index').success(function (data) {
-//       console.log(data)
-//     })
-//   }
-//   return factory;
-// });
-// myApp.controller('homeController', function ($scope, homeFactory) {
-//   $scope.getYelp = function (){
-//     console.log('controller answer')
-//     homeFactory.getYelp();
-//   }
-// })
 myApp.factory('mainFactory',function($window, $location,$http){
   var factory={};
   factory.user = {};
