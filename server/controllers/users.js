40f33c2c154afc32611638b2b9e8064abaca983d
CC Wang <chongchong1110@gmail.com>
2016-04-17 19:24:24 -0700

    using bcrypt for password safty

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
 var User = mongoose.model('User');
 module.exports = {
   create: function (req,res) {
@@ -6,55 +8,57 @@ module.exports = {
     var user = new User({ email:req.body.email, 
       password:req.body.pwd,
       fav_category: {
-        'American(New)':0,
-        'American(Tranditional)':0,
-        "BBQ":0,
+        'American(New)':1,
+        'American(Tranditional)':1,
+        "BBQ":1,
         'Brazilian Restaurant':0,
-        'Breakfast & Brunch':0,
+        'Breakfast & Brunch':1,
         'British Restaurant':0,
-        'Buffets':0,
-        'Burgers':0,
-        'Chicken Wings':0,
-        'Chinese Restaurant':0,
+        'Buffets':1,
+        'Burgers':1,
+        'Chicken Wings':1,
+        'Chinese Restaurant':1,
         'Canadian Restaurant':0,
         'Caribbean Restaurant':0,
         'Eastern European Restaurant':0,
         'Fish & Chips':0,
-        'French Restaurant':0,
+        'French Restaurant':1,
         'Filipino Restaurant':0,
-        'German Restaurant':0,
+        'German Restaurant':1,
         'Gluten - Free':0,
         'Greek Restaurant':0,
         'Hawaiian Restaurant':0, 
-        'Hot Pot':0,
+        'Hot Pot':1,
         'Hungarian Restaurant':0,
-        'Indian Restaurant':0,
+        'Indian Restaurant':1,
         'Irish Restaurant':0,
-        'Italian Restaurant':0,
-        'Japanese Restaurant':0,
-        'Korean Restaurant':0,
+        'Italian Restaurant':1,
+        'Japanese Restaurant':1,
+        'Korean Restaurant':1,
         'Latin American Restaurant':0,
         'Malaysian Restaurant':0,
         'Mediterranean Restaurant':0,
-        'Mexican Restaurant':0,
+        'Mexican Restaurant':1,
         'Middle Eastern Restaurant':0,
-        'Pizza':0,
+        'Pizza':1,
         'Russian Restaurant':0,
-        'Salad':0,
-        'Sandwiches':0,
-        'Seafood':0,
-        'Soup':0,
-        'Spanish Restaurant':0,
-        'Steakhouses':0,
-        'Sushi Bars':0,
-        'Taiwanese Restaurant':0,
-        'Thai Restaurant':0,
+        'Salad':1,
+        'Sandwiches':1,
+        'Seafood':1,
+        'Soup':1,
+        'Spanish Restaurant':1,
+        'Steakhouses':1,
+        'Sushi Bars':1,
+        'Taiwanese Restaurant':1,
+        'Thai Restaurant':1,
         'Turkish Restaurant':0,
         'Ukrainian Restaurant':0,
         'Vegan':0,
-        'Vegetarian':0,
-        'Vietnamese Restaurant':0
-      }});
+        'Vegetarian':1,
+        'Vietnamese Restaurant':1
+          },
+      restaurant:{}
+    });
     user.save(function(err,data){
       if(err){
         console.log('createing user error',err);
@@ -64,25 +68,21 @@ module.exports = {
     })
   },
   findOne:function(req,res){
-    User.findOne({'email':req.body.email},function(err,data){
+    User.findOne({'email':req.body.email}, function(err, data){
       if (err) {
-        console.log('find user errors',err);
+        console.log('find user errors', err);
       };
       if (data) {
-        //compare password using bcrypt
-        // user.comparePassword(req.body.pwd,function(err,isMatch){
-        //   if(err) throw err;
-        //   console.log(req.body.pwd, isMatch);
-        // })
-        if (data.password == req.body.pwd) {
-          res.json(data);
-        }else{
-          res.json('Email address and password do not match')
-        }
+        var user = data;
+        // compare password using bcrypt
+        bcrypt.compare(req.body.pwd, user.password, function(err, isMatch) {
+          if (err) throw err;
+          if (isMatch) res.json(user);
+          else res.json('Email address and password do not match');
+        });
       }else{
         res.json(data);
       }
-      
     })
   },
   updateCategory:function(req,res){
@@ -92,9 +92,9 @@ module.exports = {
         req.body[i]
         //mongoose way to conplie object key ==>put in query
         var query = {}
-        query['fav_category.'+ req.body[i]]=1;
+        query['fav_category.'+ req.body[i]]=2;
         User.update({_id:req.params.id},{$inc:query},function(err,data){
-            // console.log(data);
+          // console.log(data);
         })
     }
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
+
 var userSchema = new Schema({
   email:{type:String, required:true, index:{ unique: true}},
   password:{type:String, required:true},
@@ -11,31 +11,22 @@ var userSchema = new Schema({
 
 mongoose.model('User',userSchema);
 
+/* ------------- user password utils -------------- */ 
 
-// userSchema.pre('save', function(next) {
-//     var user = this;
+userSchema.pre('save', function(next) {
+  var user = this;
 
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) {
+    return next();
+  } 
 
-//     // generate a salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//         if (err) return next(err);
-
-//         // hash the password using our new salt
-//         bcrypt.hash(user.password, salt, null, function(err, hash) {
-//             if (err) return next(err);
-
-//             // override the cleartext password with the hashed one
-//             user.password = hash;
-//             next();
-//         });
-//     });
-// });
-
-// userSchema.methods.comparePassword = function(candidatePassword, cb) {
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//         if (err) return cb(err);
-//         cb(null, isMatch);
-//     });
-// };
+  // update password with hash
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
